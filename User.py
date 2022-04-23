4353d4f7d686d2be08fb066c6789c615ad7c11b4
maureenndiema <ndiemam@gmail.com>
2022-04-23 11:04:40 +0300

    add object

--- User.py
+++ User.py
@@ -8,4 +8,10 @@ class User:
     def _init_(self, user_name,password):
         self.user_name = user_name
         self.password = password
+
+    def save_user(self):
+
+        '''
+        save_user method saves a new user objects to the user_list
+        '''
         
