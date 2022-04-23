f065223ef6fd426d9164c9acf475a8349c3b9378
maureenndiema <ndiemam@gmail.com>
2022-04-23 23:58:31 +0300

    add fifth test

--- user_test.py
+++ user_test.py
@@ -64,6 +64,22 @@ def test_delete_user(self):
     self.new_user.delete_user()# Deleting a user object
     self.assertEqual(len(User.user_list),1)
 
+def test_find_user_by_number(self):
+        '''
+        test to check if we can find a user by phone number and display information
+        '''
+
+        self.new_user.save_user()
+        test_user = User("Test","user","0710909747","test@user.com") # new user
+        test_user.save_user()
+
+        found_user = User.find_by_number("0710909747")
+
+        self.assertEqual(found_user.email,test_user.email)
+
+if __name__ == '__main__':
+  unittest.main()
+
 
 
 
