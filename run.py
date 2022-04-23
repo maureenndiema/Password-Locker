5fc92207c504cf0ad3d13c37fd70254d907ee82e
maureenndiema <ndiemam@gmail.com>
2022-04-23 21:08:47 +0300

    import unittest

--- run.py
+++ run.py
@@ -1,20 +1,21 @@
-import random
-from User import User
-from Credential import Credential
+# import random
+# from User import User
+# from Credential import Credential
 
-def create_user(user_name, password):
+# def create_user(user_name, password):
 
- '''
- Function to create a new account
- '''
+#  '''
+#  Function to create a new account
+#  '''
 
- new_user = User(user_name, password)
- return new_user
+#  new_user = User(user_name, password)
+#  return new_user
 
- def save_users(user):
+#  def save_users(user):
 
-  '''
-  Function to save a user account
-  '''
+#   '''
+#   Function to save a user account
+#   '''
+  
 
  
---
+++ user_test.py
@@ -0,0 +1,15 @@
+import unittest # Importing the unittest module
+from user import User # Importing the user class
+
+class TestUser(unittest.TestCase):
+     '''
+    Test class that defines test cases for the user class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+   
+    # First Test
+    
+
+
