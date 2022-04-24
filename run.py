0659c19221721d630ecddb728c1ee87e6b48c759
maureenndiema <ndiemam@gmail.com>
2022-04-24 17:45:10 +0300

    create account for run.py

---
+++ run.py
@@ -0,0 +1,12 @@
+from Credential import Credential
+from user import User
+import random
+
+
+def create_useraccount(username, password):
+    '''
+    method creates a user account
+    '''
+    new_user = User(username, password)
+    return new_user
+
